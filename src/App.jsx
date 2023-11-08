15ee34653df02569a5f40e7190c5c19da706c0ea
rahmanmue <rahman.muraman@gmail.com>
2023-11-08 20:39:08 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,65 @@
-// import {
-//   Home,
-//   Login,
-//   Register,
-//   Dashboard,
-//   Coba,
-//   AntrianMedis,
-//   ListAkun,
-//   RiwayatPasien,
-//   RekamMedis,
-//   AkunSaya,
-//   EditAkun,
-//   TambahRekamMedis,
-// } from "./pages";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Routes from "./routes/Index";
+import {
+  Home,
+  Login,
+  Register,
+  Dashboard,
+  Coba,
+  ListAkun,
+  RekamMedis,
+  RiwayatPasien,
+  AkunSaya,
+  AntrianMedis,
+  EditAkun,
+  TambahRekamMedis,
+  EditRekamMedis,
+} from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  ProtectedRoutes,
+  ProtectedRoutesAdminDokter,
+  ProtectedRoutesAdmin,
+  PublicRoute,
+} from "./routes/PrivateRoutes";
 
 function App() {
-  return <Routes />;
-  // return (
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route index element={<AkunSaya />} />
-  //       <Route path="/home" element={<Home />} />
-  //       <Route path="/list-akun" element={<ListAkun />} />
-  //       <Route path="/riwayat-pasien" element={<RiwayatPasien />} />
-  //       <Route path="/akun-saya" element={<AkunSaya />} />
-  //       <Route path="/edit-akun" element={<EditAkun />} />
-  //       <Route path="/tambah-rekam-medis" element={<TambahRekamMedis />} />
-  //       <Route path="/rekam-medis" element={<RekamMedis />} />
-  //       <Route path="/login" element={<Login />} />
-  //       <Route path="/register" element={<Register />} />
-  //       <Route path="/dashboard" element={<Dashboard />} />
-  //       <Route path="/coba" element={<Coba />} />
-  //       <Route path="/antrian-medis" element={<AntrianMedis />} />
-  //     </Routes>
-  //   </BrowserRouter>
-  // );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/coba" element={<Coba />} />
+
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/rekam-medis/:uuid" element={<RekamMedis />} />
+          <Route path="/akun-saya" element={<AkunSaya />} />
+        </Route>
+
+        <Route element={<ProtectedRoutesAdmin />}>
+          <Route path="/list-akun" element={<ListAkun />} />
+          <Route path="/list-akun/edit/:id" element={<EditAkun />} />
+        </Route>
+
+        <Route element={<ProtectedRoutesAdminDokter />}>
+          <Route path="/riwayat-pasien" element={<RiwayatPasien />} />
+          <Route
+            path="/rekam-medis/tambah/:nama/:uuid"
+            element={<TambahRekamMedis />}
+          />
+          <Route
+            path="/rekam-medis/edit/:nama/:id/:uuid"
+            element={<EditRekamMedis />}
+          />
+          <Route path="/antrian-medis" element={<AntrianMedis />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
