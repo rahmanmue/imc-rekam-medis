c6a4a51a5c81480acc9c82eb1438f4f2b0487b48
rahmanmue <rahman.muraman@gmail.com>
2023-11-08 20:45:37 +0700

    update component

--- src/components/layout/LayoutFront.jsx
+++ src/components/layout/LayoutFront.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function LayoutFront({ children, logo }) {
   const year = new Date().getFullYear();
