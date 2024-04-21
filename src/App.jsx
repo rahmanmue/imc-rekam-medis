da7e25520db26609f83abeaf7c3aa300ccaaa7a1
rahman.muraman <rahman.muraman@gmail.com>
2024-04-21 15:25:04 +0700

    refactor: mengubah router

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
   EditRekamMedis,
   NotFound,
 } from "./pages";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createHashRouter, RouterProvider } from "react-router-dom";
 import {
   ProtectedRoutes,
   ProtectedRoutesAdminDokter,
@@ -21,46 +21,100 @@ import {
   PublicRoute,
 } from "./routes/PrivateRoutes";
 
+
+const routes = [
+  // Rute indeks
+  { path: "/", element: <Home /> },
+
+  // Rute publik
+  { 
+    element: <PublicRoute />,
+    children: [
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> }
+    ]
+  },
+
+  // Rute yang memerlukan autentikasi
+  { 
+    element: <ProtectedRoutes />,
+    children: [
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/rekam-medis/:id_user", element: <RekamMedis /> },
+      { path: "/akun-saya", element: <AkunSaya /> }
+    ]
+  },
+
+  // Rute admin
+  { 
+    element: <ProtectedRoutesAdmin />,
+    children: [
+      { path: "/list-akun", element: <ListAkun /> },
+      { path: "/list-akun/edit/:id_user", element: <EditAkun /> }
+    ]
+  },
+
+  // Rute admin dokter
+  { 
+    element: <ProtectedRoutesAdminDokter />,
+    children: [
+      { path: "/riwayat-pasien", element: <RiwayatPasien /> },
+      { path: "/rekam-medis/tambah/:nama/:id_user", element: <TambahRekamMedis /> },
+      { path: "/rekam-medis/edit/:nama/:id_rekam_medis/:id_user", element: <EditRekamMedis /> },
+      { path: "/antrian-medis", element: <AntrianMedis /> }
+    ]
+  },
+
+  // Rute untuk menangani rute yang tidak ditemukan (404)
+  { path: "*", element: <NotFound /> }
+];
+
+const router = createHashRouter(routes)
+
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/" element={<Home />} />
 
-        <Route element={<PublicRoute />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
+  return <RouterProvider router={router} />
 
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/rekam-medis/:id_user" element={<RekamMedis />} />
-          <Route path="/akun-saya" element={<AkunSaya />} />
-        </Route>
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route index element={<Home />} />
+  //       <Route path="/" element={<Home />} />
 
-        <Route element={<ProtectedRoutesAdmin />}>
-          <Route path="/list-akun" element={<ListAkun />} />
-          <Route path="/list-akun/edit/:id_user" element={<EditAkun />} />
-        </Route>
+  //       <Route element={<PublicRoute />}>
+  //         <Route path="/login" element={<Login />} />
+  //         <Route path="/register" element={<Register />} />
+  //       </Route>
 
-        <Route element={<ProtectedRoutesAdminDokter />}>
-          <Route path="/riwayat-pasien" element={<RiwayatPasien />} />
-          <Route
-            path="/rekam-medis/tambah/:nama/:id_user"
-            element={<TambahRekamMedis />}
-          />
-          <Route
-            path="/rekam-medis/edit/:nama/:id_rekam_medis/:id_user"
-            element={<EditRekamMedis />}
-          />
-          <Route path="/antrian-medis" element={<AntrianMedis />} />
-        </Route>
+  //       <Route element={<ProtectedRoutes />}>
+  //         <Route path="/dashboard" element={<Dashboard />} />
+  //         <Route path="/rekam-medis/:id_user" element={<RekamMedis />} />
+  //         <Route path="/akun-saya" element={<AkunSaya />} />
+  //       </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  //       <Route element={<ProtectedRoutesAdmin />}>
+  //         <Route path="/list-akun" element={<ListAkun />} />
+  //         <Route path="/list-akun/edit/:id_user" element={<EditAkun />} />
+  //       </Route>
+
+  //       <Route element={<ProtectedRoutesAdminDokter />}>
+  //         <Route path="/riwayat-pasien" element={<RiwayatPasien />} />
+  //         <Route
+  //           path="/rekam-medis/tambah/:nama/:id_user"
+  //           element={<TambahRekamMedis />}
+  //         />
+  //         <Route
+  //           path="/rekam-medis/edit/:nama/:id_rekam_medis/:id_user"
+  //           element={<EditRekamMedis />}
+  //         />
+  //         <Route path="/antrian-medis" element={<AntrianMedis />} />
+  //       </Route>
+
+  //       <Route path="*" element={<NotFound />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
